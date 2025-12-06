31c0c14100a49781f7a2369fcffa8afeda72c741
Frigaa Abderrahmane <frigaaabdou23@gmail.com>
2025-12-06 20:31:36 +0100

    Enhance menu UI and allow graphic mode defaults

--- main.cpp
+++ main.cpp
@@ -6,26 +6,30 @@
 int main(int argc, char** argv) {
     SimulationConfig config;
 
-    if (argc < 3) {
+    if (argc < 2) {
         std::cout << "Usage:\n"
                   << "  " << argv[0] << " console <input_file> <baseName> [maxIter]\n"
-                  << "  " << argv[0] << " graphic <input_file> [maxIter]\n";
+                  << "  " << argv[0] << " graphic [input_file] [maxIter]\n";
         return 0;
     }
 
     std::string mode = argv[1];
-    config.inputFile = argv[2];
 
     if (mode == "console") {
+        if (argc < 4) {
+            std::cout << "Usage console: " << argv[0]
+                      << " console <input_file> <baseName> [maxIter]\n";
+            return 0;
+        }
+        config.inputFile = argv[2];
         config.graphicMode = false;
-        if (argc >= 4) config.outputBaseName = argv[3];
-        else config.outputBaseName = "output";
-
+        config.outputBaseName = argv[3];
         if (argc >= 5) config.maxIterations = std::stoi(argv[4]);
 
         ConsoleRunner::run(config);
     }
     else if (mode == "graphic") {
+        if (argc >= 3) config.inputFile = argv[2];
         config.graphicMode = true;
         if (argc >= 4) config.maxIterations = std::stoi(argv[3]);
         GraphicRunner::run(config);
